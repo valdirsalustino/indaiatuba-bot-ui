cc29229a243c1e1e78e041ed1d516b3ed627fdbd
Valdir Salustino <valdirsalustino@gmail.com>
2026-01-18 23:02:17 -0300

    Revert "feat: Add client requirement improvements"

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // 1. Increase the warning limit slightly if the split chunks are still large
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        // 2. Separate third-party libraries into a "vendor" chunk
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
 })
+
